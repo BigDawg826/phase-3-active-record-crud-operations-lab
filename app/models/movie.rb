297eb75cde414f2e67dcd0e20876aa9ba598a142
Darian Fultz <dfultz826@yahoo.com>
2023-02-26 13:40:43 -0500

    Completed

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,49 @@
 class Movie < ActiveRecord::Base
 
+    def Movie.create_with_title(title)
+        Movie.create(title: title)
+    end
+
+    def Movie.first_movie
+        Movie.first 
+    end
+
+    def Movie.last_movie
+        Movie.last 
+    end
+
+    def Movie.movie_count
+        Movie.count
+    end
+
+    def Movie.find_movie_with_id(id)
+        Movie.find(id)
+    end
+
+    def Movie.find_movie_with_attributes(attr)
+        Movie.find_by(attr)
+    end
+
+    def Movie.find_movies_after_2002
+        Movie.where("release_date > 2002")
+    end
+
+    def update_with_attributes(attr)
+        self.update(attr)
+    end
+
+    def Movie.update_all_titles(title)
+        Movie.update(title: title)
+    end
+
+    def Movie.delete_by_id(id)
+        Movie.delete(id)
+    end
+
+    def Movie.delete_all_movies
+        Movie.destroy_all
+    end
+
+
+
 end
